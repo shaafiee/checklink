28493f98d7d138e7fd5587c77cc9f856308183ff
Shaafiee <mohamed.shaafiee@comfort-works.com>
2025-03-26 07:42:42 +0000

    refresh

--- checklink.py
+++ checklink.py
@@ -1,17 +1,40 @@
 import requests
 import sys
+import traceback
+import json
+import re
 
 def checkList(file, prefix):
 	with open(file) as IF:
+		incorrect = []
 		counter = 0
+		success = 0
+		fail = 0
 		for line in IF:
+			(origin, redirect) = line.split(",")
+			#origin = f"{prefix}{origin}"
+			redirect = redirect.replace("\n", "")
+			try:
+				returned = requests.get(f"{prefix}{origin}", allow_redirects=True)
+				if re.match(r"^.*" + redirect + ".*", returned.url):
+					success += 1
+					print(f"[OK] {origin} --> {redirect}")
+				else:
+					incorrect.append({"origin": origin, "redirect": redirect})
+					fail += 1
+					print(f"[ERROR] {origin} --> {redirect}")
+			except:
+				fail += 1
+				pass
 			counter += 1
-			(origin, redirect) = line.split(f"\s*,\s*")
-			print(f"{origin} --> {redirect}")
-			returned = requests.get(f"{prefix}{origin}", allow_redirects=True)
-			print(returned.url)
-			if counter > 10:
-				exit()
+		successRate = int((success / counter) * 100)
+		print("\n")
+		print(f"{successRate}% success")
+		if successRate < 100:
+			print(f"Check errors.json")
+		print("\n")
+		with open("errors.json", "w") as WF:
+			WF.write(json.dumps(incorrect))
 
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
@@ -22,4 +45,7 @@ if __name__ == "__main__":
 	try:
 		checkList(sys.argv[1], sys.argv[2] if len(sys.argv) == 3 else "")
 	except:
-		print("Error")
+		print("=========================ERROR!=========================")
+		print("========================================================")
+		print(traceback.format_exc())
+		print("========================================================")
